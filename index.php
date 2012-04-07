d0ac204f7ef369c5064d132879a84e82bda81221
leoman730 <leoman730@gmail.com>
2012-04-06 21:56:19 -0400

    add google analytics

--- index.php
+++ index.php
@@ -28,6 +28,21 @@
 		     For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
 		<script src="resources/js/libs/modernizr.min.js"></script>
 	
+	<!--add google analytics-->
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-30665798-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+	
 	</head>
 	
 	<body class="light">
